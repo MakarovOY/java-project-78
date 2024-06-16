732ea9e5e9e53dae25110a2d8b6d8803c72d99e4
Oleg Makarov <makarovolegbox@yandex.ru>
2024-06-16 16:37:19 +0300

    Merge remote-tracking branch 'origin/main'

--- app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/schemas/MapSchemaTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
-import java.util.Map;
+
 
 import static org.assertj.core.api.Assertions.assertThat;
 
